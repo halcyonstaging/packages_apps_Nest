ffb2342e3fc4d14cb925091f359f431fbf918396
Pranav Vashi <neobuddy89@gmail.com>
2023-12-27 22:42:30 +0530

    Nest: Prevent hotseat QSB crash with google stub
    Fixes: https://github.com/crdroidandroid/issue_tracker/issues/286
    Change-Id: Iba30d8e2fc316810e57e09b69bf4085fff64f190
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/com/android/launcher3/qsb/AssistantIconView.java
+++ src/com/android/launcher3/qsb/AssistantIconView.java
@@ -21,12 +21,16 @@ public class AssistantIconView extends ImageView {
     }
 
     public void setListener(Context context) {
-        setOnClickListener(view -> {
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            intent.setAction("android.intent.action.VOICE_COMMAND");
-            intent.setPackage(QsbContainerView.getSearchWidgetPackageName(context));
-            context.startActivity(intent);
-        });
+        try {
+            setOnClickListener(view -> {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                intent.setAction("android.intent.action.VOICE_COMMAND");
+      	        intent.setPackage(QsbContainerView.getSearchWidgetPackageName(context));
+	        context.startActivity(intent);
+            });
+        } catch (Exception e) {
+            // Do nothing
+        }
     }
 }
